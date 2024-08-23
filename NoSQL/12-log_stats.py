85cf1e80dc423173ef1fb6187dde1a59149d1aba
cely <146735843+cel3788@users.noreply.github.com>
2024-08-23 23:53:27 +0200

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,31 +1,48 @@
-from pymongo import MongoClient
+!/usr/bin/env python3
+"""Script that returns Nginx logs stats stored in MongoDB"""
+import pymongo
 
-def log_stats():
-    # Connect to the MongoDB server
-    client = MongoClient('localhost', 27017)
-    
-    # Access the logs database and the nginx collection
-    db = client.logs
-    collection = db.nginx
-    
-    # Get the total number of documents
-    total_logs = collection.count_documents({})
-    
-    # Count the number of documents for each HTTP method
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {}
-    for method in methods:
-        method_counts[method] = collection.count_documents({"method": method})
-    
-    # Count the number of documents with method=GET and path=/status
-    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
-    
-    # Print the results
-    print(f"{total_logs} logs")
+"""
+client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+database = client['logs']
+collection = database['nginx']
+
+logs = collection.count_documents({})
+print(f"{logs} logs")
+
+all_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+methods = {method: collection.count_documents(
+    {"method": method}) for method in all_methods}
+print("Methods:")
+for method, count in methods.items():
+    print(f"    method {method}: {count}")
+
+status = collection.count_documents({"method": "GET", "path": "/status"})
+print(f"{status} status check")
+
+client.close()
+"""
+
+
+def collection(db: dict) -> int:
+    """Function to retroeve logs information"""
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    return logs.count_documents(db)
+
+
+def main():
+    """Function that returns stats about Nginx logs stored in MongoDB"""
+
+    print(f"{collection({})} logs")
     print("Methods:")
-    for method in methods:
-        print(f"\tmethod {method}: {method_counts[method]}")
-    print(f"{status_check_count} status check")
+    print(f"\tmethod GET: {collection({'method': 'GET'})}")
+    print(f"\tmethod POST: {collection({'method': 'POST'})}")
+    print(f"\tmethod PUT: {collection({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {collection({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {collection({'method': 'DELETE'})}")
+    print(f"{collection({'method': 'GET', 'path': '/status'})} status check")
+
 
 if __name__ == "__main__":
-    log_stats()
+    main()
