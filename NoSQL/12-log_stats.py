2516f65fabc8df1d04bcfa0e91401c5311c543cd
cely <146735843+cel3788@users.noreply.github.com>
2024-08-23 23:56:16 +0200

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,17 +7,19 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
+    # Count total number of logs
     n_logs = nginx_collection.count_documents({})
     print(f'{n_logs} logs')
 
+    # Count logs for each HTTP method
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
         print(f'\tmethod {method}: {count}')
 
+    # Count the number of GET requests to /status
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-
     print(f'{status_check} status check')
