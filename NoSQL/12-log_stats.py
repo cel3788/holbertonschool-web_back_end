eec9abb1d5fe92d45afc4ac53e108b301c3dd551
cely <146735843+cel3788@users.noreply.github.com>
2024-08-23 23:44:56 +0200

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
-"""Log statistics"""
+"""Provide statistics about Nginx logs stored in MongoDB."""
+
 from pymongo import MongoClient
 
-
-def log(a: dict) -> int:
-    """return the log"""
+def get_log_count(query: dict) -> int:
+    """Return the count of logs that match the given query."""
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_logs = client.logs.nginx
-    return nginx_logs.count_documents(a)
-
+    return nginx_logs.count_documents(query)
 
 def print_nginx_logs_stats():
-    """Provide statistics for stored Nginx logs"""
-    print(f"{ log({}) } logs")
+    """Print statistics about the Nginx logs stored in MongoDB."""
+    total_logs = get_log_count({})
+    print(f"{total_logs} logs")
+    
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
-    print(f"\tmethod GET: { log({'method': 'GET'}) }")
-    print(f"\tmethod POST: { log({'method': 'POST'}) }")
-    print(f"\tmethod PUT: {log({'method': 'PUT'})}")
-    print(f"\tmethod PATCH: {log({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {log({'method': 'DELETE'})}")
-    print(f"{log({'method': 'GET', 'path': '/status'})} status check")
-
+    for method in methods:
+        count = get_log_count({'method': method})
+        print(f"\tmethod {method}: {count}")
+    
+    status_check_count = get_log_count({'method': 'GET', 'path': '/status'})
+    print(f"{status_check_count} status check")
 
 if __name__ == "__main__":
     print_nginx_logs_stats()
